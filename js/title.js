972937fff10da8c89f5bdf4994c31cdb70384578
李一诺 <liyinuo@dailyinnovation.biz>
2024-09-25 11:58:56 +0800

    Site updated: 2024-09-25 11:58:55

--- js/title.js
+++ js/title.js
@@ -4,14 +4,14 @@ var titleTime;
 document.addEventListener("visibilitychange", function () {
   if (document.hidden) {
     //离开当前页面时标签显示内容
-    document.title = "w(ﾟДﾟ)w 不再看看嘛？";
-    clearTimeout(titleTime);
+    // document.title = "w(ﾟДﾟ)w 不再看看嘛？";
+    // clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
-    document.title = "♪(^∇^*)欢迎回来！" + OriginTitile;
-    //两秒后变回正常标题
-    titleTime = setTimeout(function () {
-      document.title = OriginTitile;
-    }, 2000);
+    // document.title = "♪(^∇^*)欢迎回来！" + OriginTitile;
+    // //两秒后变回正常标题
+    // titleTime = setTimeout(function () {
+    //   document.title = OriginTitile;
+    // }, 2000);
   }
 });
